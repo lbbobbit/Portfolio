c779c705e076d216fba278a4e006e767053b76db
lbbobbit <hugsoverdrugs@yahoo.com>
2021-10-11 23:55:34 -0400

    Add files via upload

---
+++ quiz1.js
@@ -0,0 +1,91 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+ 
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+ 
+    this.questionIndex++;
+}
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+ 
+ 
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+ 
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+ 
+ 
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+ 
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+ 
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+ 
+// create questions here
+var questions = [
+    new Question("What type of animal is a Flemish giant?", ["Rabbit", "Horse","Squirrel, "Trijntje Keever"], "Rabbit"),
+    new Question("The unicorn is the national animal of which country?", ["Russia", "Canada", "Ghana", "Scotland"], "Scotland"),
+    new Question("What sport is dubbed the "king of sports"?", ["Soccer", "Jai Alai","Basketball", "Badminton"], "Soccer"),
+    new Question("Who created Sherlock Holmes?", ["J.R. Tolkien", "Arthur Conan Doyle", "Robert Ludlum", "Karin Slaughter"], "Arthur Conan Doyle"),
+    new Question("What was the first toy to be advertised on television?", ["Slinky", "Cabbage Patch Kid", "Mr. Potato Head", "Simon"], "Mr. Potato Head")
+];
+ 
+// create quiz
+var quiz = new Quiz(questions);
+ 
+// display quiz
+populate();
